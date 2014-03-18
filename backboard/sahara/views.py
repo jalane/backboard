1cb4c0230f5a3abfabc72ee739dbdeba500fbc03
Lane <jalane@bos-mptlm.kendall.corp.akamai.com>
2014-03-18 15:07:18 -0400

    Dynamic Code generation test

--- backboard/sahara/views.py
+++ backboard/sahara/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-# Create your views here.
 def index(request):
-    module = imp.new_module('mymodule')
-    
     thecode = """
     return HttpResponse("Hello, this is the dyanmic code executor")
     """
     
-    exec thecode in module.__dict__
-
-    application = webapp.WSGIApplication([('/', module.MainHandler)], debug=True)
-    util.run_wsgi_app(application)
+    exec thecode 
