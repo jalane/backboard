43305dc7f9e67fed7512df1e8c51d2aa6cc476c7
Lane <jalane@bos-mptlm.kendall.corp.akamai.com>
2014-03-18 15:12:56 -0400

    Dynamic Code generation test

--- backboard/sahara/views.py
+++ backboard/sahara/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    thecode = "return HttpResponse(\"Hello, this is the dyanmic code executor\")"
+    thecode = "return HttpResponse('Hello, this is the dyanmic code executor')"
     exec thecode 
